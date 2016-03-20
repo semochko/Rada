0ed847d975f4ec7593e72932698178fd6f28f5aa
i1kondratiuk <i1kondratiuk@gmail.com>
2016-03-20 13:31:19 +0200

    added List for test

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
 package rada;
 
-public class Main {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Main {
+	public static void main(String[] args) {
+		List<Deputy> deputies = new ArrayList<>();
+		deputies.add(new Deputy());
+		deputies.add(new Deputy());
+
+		for (Deputy deputy : deputies) {
+			System.out.println(deputy.name + ", " + deputy.gender + ", " + deputy.age + ", " + deputy.grafter);
+		}
+	}
 }
