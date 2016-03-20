7d14b2300426a08342845e97e4306f2ca3d09ece
i1kondratiuk <i1kondratiuk@gmail.com>
2016-03-20 10:28:57 +0200

    fields

--- Deputy.java
+++ Deputy.java
@@ -7,17 +7,18 @@ package rada;
  */
 
 public class Deputy {
-	private String name;
-	private String gender;
-	private int age;
-	private boolean grafter;
+	String name;
+	String gender;
+	int age;
+	boolean grafter;
 
 	public Deputy(String name, String gender, int age, int bride) {
 		this.name = name;
 		this.gender = gender;
 		this.age = age;
 		if(bride > 50_000) {
-			System.err.println("Deputy" + name + " was caught on a bribe of $ "+ bride + ". It has to be prisoners.");
+			grafter = true;
+			System.out.println("Deputy" + name + " was caught on a bribe of $ "+ bride + ". It has to be prisoners.");
 		} else if (bride > 0) {
 			grafter = true;
 		} else {
